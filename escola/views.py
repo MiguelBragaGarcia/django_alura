986291a97694df8e39e1da6a072e7591c3f067ba
Miguel Braga Garcia <miguelbragagarcia@gmail.com>
2021-08-02 14:22:39 -0300

    Adição da rota de matrícula.

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializers import AlunoSerializer, CursoSerializer
+from escola.models import Aluno, Curso, Matricula
+from escola.serializers import AlunoSerializer, CursoSerializer, MatriculaSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos"""
@@ -10,3 +10,9 @@ class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset= Curso.objects.all()
     serializer_class=CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset= Matricula.objects.all()
+    serializer_class=MatriculaSerializer
+
