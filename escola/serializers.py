6b0617fc228464be6e2288847cb6e9dd17b795b6
Miguel Braga Garcia <miguelbragagarcia@gmail.com>
2021-08-04 11:07:21 -0300

    Adição das rotas de listar todos os cursos de um aluno e de listar os alunos cadastrados em um curso.

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,4 +1,3 @@
-from django.db.models import fields
 from rest_framework import serializers
 from escola.models import Aluno, Curso, Matricula
 
@@ -16,3 +15,19 @@ class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         exclude = []
+
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome  = serializers.ReadOnlyField(source='aluno.nome')
+    class Meta:
+        model = Matricula
+        fields= ['aluno_nome']
--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from escola.serializers import AlunoSerializer, CursoSerializer, MatriculaSerializer
+from escola.serializers import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos"""
@@ -16,3 +16,16 @@ class MatriculaViewSet(viewsets.ModelViewSet):
     queryset= Matricula.objects.all()
     serializer_class=MatriculaSerializer
 
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listanto todas as matrículas de um aluno"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id= self.kwargs["pk"])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+
+class ListaAlunoMatriculados(generics.ListAPIView):
+    """Listando alunos matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
