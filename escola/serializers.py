5d38ce8d71551f8ed022f17efa81599c07684fed
Miguel Braga Garcia <miguelbragagarcia@gmail.com>
2021-08-17 09:17:01 -0300

    Adição de conversor de campos na serialização.

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,15 +1,33 @@
 from rest_framework import serializers
 from escola.models import Aluno, Curso, Matricula
 
+class ChoiceField(serializers.ChoiceField):
+
+    def to_representation(self, obj):
+        if obj == '' and self.allow_blank:
+            return obj
+        return self._choices[obj]
+
+    def to_internal_value(self, data):
+        # To support inserts with the value
+        if data == '' and self.allow_blank:
+            return ''
+
+        for key, val in self._choices.items():
+            if val == data:
+                return key
+        self.fail('invalid_choice', input=data)
+
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
 
 class CursoSerializer(serializers.ModelSerializer):
+    nivel = ChoiceField(choices=Curso.NIVEL)
     class Meta:
         model =Curso
-        fields='__all__'
+        fields= "__all__"
 
 class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
